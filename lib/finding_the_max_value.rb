6ad6022d2705741425ab56cddb9ee7fbffdc0ded
stephen pesce <stevepesce879@gmail.com>
2020-04-28 19:56:44 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,13 @@
 def find_max_value(array)
-  new_max;
+  new_max = -1;
   
   array.each
   {
-    |n| if 
+    |n| 
+    
+    if array[n] > new_max
+      new_max = array[n]
+    end
   }
+  new_max
 end  
