0e37e15f38a0d43aacc2b6a976621b8800e637fe
stephen pesce <stevepesce879@gmail.com>
2020-04-28 20:07:05 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,16 +1,12 @@
+//returns max value from unsigned int array 
 def find_max_value(array)
-  puts "function begin"
   new_max = -1;
   
   array.each { |n| 
-    
     puts n 
     if n > new_max
       new_max = n
-      puts "new_max = #{new_max}"
     end
   }
-  
-  puts "function end"
   new_max
 end  
